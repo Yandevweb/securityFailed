5876ee43cda5b91b18f620dd04159a76b2b96e29
Yandevweb <yannick.jeanjean.pro@gmail.com>
2018-04-11 10:38:28 +0200

    merge

--- app/index.php
+++ app/index.php
@@ -10,12 +10,14 @@ $result = mysqli_query($mysqli,"CREATE DATABASE IF NOT EXISTS security_woot;");
 $result = mysqli_query($mysqli,"CREATE TABLE IF NOT EXISTS comments (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30) NOT NULL, content TEXT NOT NULL);");
 
 $mysqli->close();
-
 $mysqli = mysqli_connect('mysql', 'root', 'root', 'security_woot');
 
-
-
 $comment = isset($_POST['comment']) && !empty($_POST['comment']) ? $_POST['comment'] : false;
+
+
+// Récuperation des commentaires
+$comments = mysqli_query($mysqli, "SELECT * FROM comments ;");
+
 
 
 ?>
@@ -32,6 +34,11 @@ $comment = isset($_POST['comment']) && !empty($_POST['comment']) ? $_POST['comme
         <textarea name="comment" id="" cols="30" rows="10"></textarea>
     </form>
 
+  <?php foreach ($comments as $com): ?>
+    <div>
+        <?= $com ?>
+    </div>
+  <?php endforeach; ?>
 
   </body>
 </html>
