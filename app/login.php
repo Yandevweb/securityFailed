3c0a06e440cf902fa4aee0f0c1ef7bc9175a45e1
Angeles M <angeles.morap@gmail.com>
2018-04-11 16:23:41 +0200

    Add side-nav class

--- app/login.php
+++ app/login.php
@@ -17,7 +17,7 @@
 <body>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-2">
+      <div class="col-2 side-nav">
         <?php include('./partials/nav.php');?>
       </div>
       <div class="col">
