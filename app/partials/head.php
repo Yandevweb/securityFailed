18ad2e55a6f71878bfea7ed577e35b728d71e549
Angeles M <angeles.morap@gmail.com>
2018-04-11 15:34:21 +0200

    Add font & home URL to side nav

--- app/partials/head.php
+++ app/partials/head.php
@@ -7,9 +7,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
           crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
     <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i" rel="stylesheet">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
             integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
             crossorigin="anonymous"></script>
--- app/partials/nav.php
+++ app/partials/nav.php
@@ -1,4 +1,5 @@
 <div class="btn-group-toggle">
+  <a href="index.php" class="btn btn-secondary btn-sm">Book Challenge</a>
   <a href="login.php" class="btn btn-secondary btn-sm active">Header challenge</a>
   <a href="cookie.php" class="btn btn-secondary btn-sm active">Cookie challenge</a>
 </div>
