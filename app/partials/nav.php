b390fc9b0c3530c3b539e5f961df23f95769d0ad
Angeles M <angeles.morap@gmail.com>
2018-04-11 15:09:11 +0200

    Add partials and css files

---
+++ app/partials/nav.php
@@ -0,0 +1,4 @@
+<div class="btn-group-toggle">
+  <a href="login.php" class="btn btn-secondary btn-sm active">Header challenge</a>
+  <a href="cookie.php" class="btn btn-secondary btn-sm active">Cookie challenge</a>
+</div>
