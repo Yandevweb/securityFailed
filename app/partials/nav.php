537299ae7414759cc13244e7c7f5834670e39ac4
Angeles M <angeles.morap@gmail.com>
2018-04-11 15:50:42 +0200

    Nav bar

--- app/partials/nav.php
+++ app/partials/nav.php
@@ -1,3 +1,4 @@
+<span>Other challenges you can try:</span>
 <div class="btn-group-toggle">
   <a href="index.php" class="btn btn-secondary btn-sm">Book Challenge</a>
   <a href="login.php" class="btn btn-secondary btn-sm active">Header challenge</a>
