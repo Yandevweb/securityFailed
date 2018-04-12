d1d6fd561d40a897b4563052bca958c805b4e302
Angeles M <angeles.morap@gmail.com>
2018-04-12 10:26:00 +0200

    Nasty fix

--- app/partials/head.php
+++ app/partials/head.php
@@ -4,6 +4,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/app.css">
+    <!-- Quick and dirty fix :/ -->
+    <link rel="stylesheet" href="../css/app.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
           crossorigin="anonymous">
